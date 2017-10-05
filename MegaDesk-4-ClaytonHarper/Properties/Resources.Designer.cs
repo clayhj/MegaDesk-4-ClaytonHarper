7ca3fa9d6ef5754e573658a6c6691fff552bfba8
Clayton Harper <har12115@byui.edu>
2017-10-05 09:45:35 -0600

    initial

--- MegaDesk-4-ClaytonHarper/Properties/Resources.Designer.cs
+++ MegaDesk-4-ClaytonHarper/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MegaDesk_3_ClaytonHarper.Properties {
+namespace MegaDesk_4_ClaytonHarper.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MegaDesk_3_ClaytonHarper.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MegaDesk_3_ClaytonHarper.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MegaDesk_4_ClaytonHarper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
